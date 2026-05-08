05e148e618fb6ce8f7229bf6dfe0436de50a45e7
Александр Мискин <near.ai@yandex.com>
2026-05-08 20:35:04 +0300

    Added simple example of usage DynamicLock

---
+++ example/main.cpp
@@ -0,0 +1,31 @@
+#include "dynamic/dynamic_lock.hpp"
+
+static constexpr int kLockSize = 1 << 20;
+static constexpr int kLocks = 64;
+static constexpr int kThreadCnt = 8;
+
+int main() {
+    std::vector<std::thread> ThreadPoll;
+
+    DynamicLock<kLocks> dynamic_lock(kLockSize);
+
+    ThreadPoll.reserve(kThreadCnt);
+    std::vector<int> data(kLockSize, 0);
+
+    for (int idx = 0; idx < kThreadCnt; ++idx) {
+        ThreadPoll.emplace_back([&dynamic_lock, &data]() {
+            for (int i = 0; i < kLockSize; ++i) {
+                dynamic_lock.WriteQuery(i, i + 1, [&data](int left, int) {
+                    ++data[left];
+                });
+            }
+        });
+    }
+    for (auto &&th: ThreadPoll) {
+        th.join();
+    }
+
+    if (!std::ranges::all_of(data, [](int value) { return value == kThreadCnt; })) {
+        throw std::runtime_error("Incorrect result");
+    }
+}
